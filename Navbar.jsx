6570d847eb4d5d02244498dd3c65468a6387e5c8
krishnaveniponnu2006-art <krishnaveniponnu2006@gmail.com>
2026-01-22 14:51:50 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,40 +1,25 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="secondary">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              BlogApp
-            </Typography>
-            <Button>
-              <Link style={{ textDecoration: "none", color: "white" }} to={"/"}>
-                Home
-              </Link>
-            </Button>
-            <Button>
-              <Link
-                style={{ textDecoration: "none", color: "white" }}
-                to={"/add"}
-              >
-                Add
-              </Link>
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            BlogApp
+          </Typography>
+
+          <Button component={Link} to="/" color="inherit">
+            Home
+          </Button>
+          <Button component={Link} to="/add" color="inherit">
+            Add
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
