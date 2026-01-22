74e984f39f62fadef528b7e1e354b7873acd1a0c
krishnaveniponnu2006-art <krishnaveniponnu2006@gmail.com>
2026-01-22 14:50:03 +0530

    Update Add.jsx

--- Add.jsx
+++ Add.jsx
@@ -1,84 +1,79 @@
+import React, { useState, useEffect } from "react";
 import { Box, Button, TextField } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Add = () => {
   const navigate = useNavigate();
-  var [inputs, setInputs] = useState({
+  const location = useLocation();
+
+  const [inputs, setInputs] = useState({
     title: "",
     content: "",
     img_url: "",
   });
-  const inputHandler = (e) => {
-    console.log(e.target.value);
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-    console.log("in",inputs);
-  };
-  const addData = () => {
-    console.log("clicked");
-    axios
-      .post("http://localhost:3001/add",inputs)
-      .then((res) => {
-        alert(res.data.message);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  return (
-    <div>
-      <div>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "90vh",
-          }}
-        >
-          <Box
-            component="form"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              width: "600px",
-            }}
-          >
-            <TextField
-              variant="outlined"
-              placeholder="Title"
-              onChange={inputHandler}
-              name="title"
-              value={inputs.title}
-              fullWidth
-            />
-            <TextField
-              variant="outlined"
-              placeholder="content"
-              onChange={inputHandler}
-              name="content"
-              value={inputs.content}
-              multiline={4}
-            />
-            <TextField
-              variant="outlined"
-              placeholder="image url"
-              onChange={inputHandler}
-              name="img_url"
-              value={inputs.img_url}
-            />
 
-            <Button variant="contained" color="secondary" onClick={addData}>
-              Submit
-            </Button>
-          </Box>
-        </Box>
-      </div>
-    </div>
+  // Fill form if editing
+  useEffect(() => {
+    if (location.state) {
+      setInputs({
+        title: location.state.title,
+        content: location.state.content,
+        img_url: location.state.img_url,
+      });
+    }
+  }, [location.state]);
+
+  const inputHandler = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (location.state) {
+        // Update existing blog
+        await axios.put(`http://localhost:3001/update/${location.state._id}`, inputs);
+        alert("Blog updated successfully");
+      } else {
+        // Add new blog
+        await axios.post("http://localhost:3001/add", inputs);
+        alert("Blog added successfully");
+      }
+      navigate("/"); // go back to Home
+    } catch (err) {
+      console.error(err);
+      alert("Operation failed");
+    }
+  };
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "90vh" }}>
+      <Box sx={{ width: "600px", display: "flex", flexDirection: "column", gap: 2 }}>
+        <TextField
+          placeholder="Title"
+          name="title"
+          value={inputs.title}
+          onChange={inputHandler}
+        />
+        <TextField
+          placeholder="Content"
+          name="content"
+          value={inputs.content}
+          onChange={inputHandler}
+          multiline
+          rows={4}
+        />
+        <TextField
+          placeholder="Image URL"
+          name="img_url"
+          value={inputs.img_url}
+          onChange={inputHandler}
+        />
+        <Button variant="contained" color="secondary" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
